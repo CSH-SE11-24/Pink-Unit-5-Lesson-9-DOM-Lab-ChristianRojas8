6d85bfa5efb9f5a632cb9c198ff70b6491357275
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:20:19 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("Script running...")
+// Task 1: Intro section
+// Implement the DOM manipulation code flow such that when you click the "Click for location" button, the text below says "1300 Boynton Ave (moving soon!)"
+// Make sure to add whatever IDs/classes you need to select the necessary elements
+let location1 = document.querySelector("#location")
+
+let p = document.querySelector("#locationText")
+
+
+
+location1.addEventListener("click", function(event){
+  p.textContent = "1300 Boynton Ave (moving soon!)"
+})
+
+
+
+
+
+// Task 2: Our Values section
+// Implement the DOM manipulation code flows for each of the SCORE <h3> tags such that when you click on them, the text changes to the full value
+// S -> Self-awareness, C -> Consistency, O -> Ownership, R -> Resilience, E -> Excellence
+
+
+
+
+
+
+
+
+
+// Task 3: Uniform section
+// Implement the DOM manipulation code flows for each of the images such that when your mouse is over the image, it displays the back image and when it is off the image, it displayes the front image
+// First image: Mouse over -> joggers-front.webp, mouse off -> joggers-back.webp
+// Second image: Mouse over -> sweater-front.webp, mouse off -> sweater-back.webp
+
+
+
+
+
+
+
+// EXTRA CREDIT
+// Implement the DOM manipulation code flows for each of the SCORE <h3> tags such that when your mouse is over them, the text color changes to #3ab54b (and back to black when it is off it)
+
+
+
+
+
